b5b5551aa94d93b64daf4cdd4bc010d22e7af9ea
Keith Horsey <keith.aich@organizdkaoz.com>
2019-02-01 11:21:56 -0800

    Adding SSM auto-install powershell file

---
+++ AmazonSSMAutoInstall/autoInstallSSM.ps1
@@ -0,0 +1,23 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.157
+	 Created on:   	1/31/2019 3:30 PM
+	 Created by:   	 Keith Horsey
+	 Organization: 	 Deluxe Entertainment Services Group
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine -Force
+
+# Set up the preliminary parameters for the script
+PowerShell param([Parameter(Mandatory)][string]$ActivationCode,
+	[Parameter(Mandatory)][string]$ActivationId,
+	[Parameter()][string]$Region = 'us-west-1')
+
+# Grab the installer file and download it to temp
+$installerFilePath = "$env:Temp.exe" if (-not (Test-Path -Path $installerFilePath)) { $iwrParams = @{ Uri = "https://amazon-ssm-$Region.s3.amazonaws.com/latest/windows_amd64/AmazonSSMAgentSetup.exe" UseBasicParsing = $true OutFile = $installerFilePath } Invoke-WebRequest @iwrParams }
+
+# Run the installer
+$procParams = @{ FilePath = $installerFilePath  ArgumentList = @('/q', '/log', "`"$env:Temp.log`"", "CODE=$ActivationCode", "ID=$ActivationId", "REGION=$Region") Wait = $true NoNewWindow = $true } Write-Host "Installing SSM agent with arguments: [$($procParams.ArgumentList)]" Start-Process @procParams
